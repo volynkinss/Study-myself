742347eb8fa51ceca069d6068ff7335117b423e9
volynkinss <volynkin_s@icloud.com>
2023-04-05 16:33:03 +0300

    Displaying even numbers in range

---
+++ exrecise_1.py
@@ -0,0 +1,11 @@
+# Displaying even numbers from 1 to 10
+
+numbers = range(1, 10)
+even_numbers = 0
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
+        even_numbers += 1
+    else:
+        continue
+print(even_numbers, "even numbers on display")
