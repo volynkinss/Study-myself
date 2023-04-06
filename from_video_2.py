85de9493f6830f502363918716037aad70d486ce
volynkinss <volynkin_s@icloud.com>
2023-04-06 17:30:55 +0300

    end of video

--- from_video_2.py
+++ from_video_2.py
@@ -1,1 +1,17 @@
-print("hello world")
+# guess = 0
+# answer = 5
+
+# while guess != answer:
+#     guess = int(input("Guess: "))
+
+
+def multiplay(*nembers):
+    total = 1
+    for number in nembers:
+        total *= number
+    return total
+
+
+print("start")
+print(multiplay(1, 2, 3))
+print("finish")
