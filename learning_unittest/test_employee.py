3dbc449a54d4d6af7137ba1bf946c57ffa5fe62c
volynkinss <volynkin_s@icloud.com>
2023-04-10 14:15:06 +0300

    created two employees over tests to delete the need to create in each test

--- learning_unittest/test_employee.py
+++ learning_unittest/test_employee.py
@@ -3,41 +3,36 @@ from employee import Employee
 
 
 class TestEmployee(unittest.TestCase):
+    def setUp(self):
+        self.emp_1 = Employee("Tim", "Birton", 50000)
+        self.emp_2 = Employee("Kate", "Williams", 100000)
+
     def test_email(self):
-        emp_1 = Employee("Tim", "Birton", 50000)
-        emp_2 = Employee("Kate", "Williams", 100000)
+        self.assertEqual(self.emp_1.email, "Tim.Birton@email.com")
+        self.assertEqual(self.emp_2.email, "Kate.Williams@email.com")
 
-        self.assertEqual(emp_1.email, "Tim.Birton@email.com")
-        self.assertEqual(emp_2.email, "Kate.Williams@email.com")
+        self.emp_1.first = "John"
+        self.emp_2.first = "Ann"
 
-        emp_1.first = "John"
-        emp_2.first = "Ann"
-
-        self.assertEqual(emp_1.email, "John.Birton@email.com")
-        self.assertEqual(emp_2.email, "Ann.Williams@email.com")
+        self.assertEqual(self.emp_1.email, "John.Birton@email.com")
+        self.assertEqual(self.emp_2.email, "Ann.Williams@email.com")
 
     def test_fullname(self):
-        emp_1 = Employee("Tim", "Birton", 50000)
-        emp_2 = Employee("Kate", "Williams", 100000)
+        self.assertEqual(self.emp_1.fullname, "Tim Birton")
+        self.assertEqual(self.emp_2.fullname, "Kate Williams")
 
-        self.assertEqual(emp_1.fullname, "Tim Birton")
-        self.assertEqual(emp_2.fullname, "Kate Williams")
+        self.emp_1.first = "John"
+        self.emp_2.first = "Ann"
 
-        emp_1.first = "John"
-        emp_2.first = "Ann"
-
-        self.assertEqual(emp_1.fullname, "John Birton")
-        self.assertEqual(emp_2.fullname, "Ann Williams")
+        self.assertEqual(self.emp_1.fullname, "John Birton")
+        self.assertEqual(self.emp_2.fullname, "Ann Williams")
 
     def test_apply_raise(self):
-        emp_1 = Employee("Tim", "Birton", 50000)
-        emp_2 = Employee("Kate", "Williams", 100000)
+        self.emp_1.apply_raise()
+        self.emp_2.apply_raise()
 
-        emp_1.apply_raise()
-        emp_2.apply_raise()
-
-        self.assertEqual(emp_1.pay, 52500)
-        self.assertEqual(emp_2.pay, 105000)
+        self.assertEqual(self.emp_1.pay, 52500)
+        self.assertEqual(self.emp_2.pay, 105000)
 
 
 if __name__ == "__main__":
